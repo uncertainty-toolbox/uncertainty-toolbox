1ece4409f0e4af447419ac44b55d41f009972b8f
willieneis <willie.neiswanger@gmail.com>
2020-11-28 15:15:25 -0800

    update imports in __init__

--- uncertainty_toolbox/__init__.py
+++ uncertainty_toolbox/__init__.py
@@ -1,3 +1,30 @@
 """
 Code for the Uncertainty Toolbox
 """
+
+from .data import (
+    synthetic_arange_random,
+    synthetic_sine_heteroscedastic,
+    curvy_cosine,
+)
+
+from .metrics import(
+    get_all_metrics,
+)
+
+from .metrics_accuracy import prediction_error_metrics
+
+from .metrics_calibration import (
+    root_mean_squared_calibration_error,
+    mean_absolute_calibration_error,
+    miscalibration_area,
+    adversarial_group_calibration,
+    sharpness,
+)
+
+from .metrics_scoring_rule import (
+    nll_gaussian,
+    crps_gaussian,
+    check_score,
+    interval_score,
+)
