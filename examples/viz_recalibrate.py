d22009e6b570874647d9f5be97ede3986a83ae2e
YoungseogChung <youngsec@cs.cmu.edu>
2020-12-06 00:49:45 -0500

    added recalibration plots, fixed recalibration code

---
+++ examples/viz_recalibrate.py
@@ -0,0 +1,135 @@
+"""
+Examples of code for visualizations.
+"""
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib import rc
+
+# plt.ion()
+
+import uncertainty_toolbox.data as udata
+import uncertainty_toolbox.metrics as umetrics
+from uncertainty_toolbox.metrics_calibration import get_proportion_lists_vectorized
+import uncertainty_toolbox.viz as uviz
+from uncertainty_toolbox.recalibration import iso_recal
+from uncertainty_toolbox.viz import plot_calibration
+
+import neatplot
+
+neatplot.set_style()
+neatplot.update_rc("text.usetex", False)
+
+# Set random seed
+np.random.seed(11)
+
+# Generate synthetic predictive uncertainty results
+n_obs = 650
+f, std, y, x = udata.synthetic_sine_heteroscedastic(n_obs)
+
+# Save figure (set to True to save)
+savefig = False
+
+
+def save_figure(name_str, file_type="png"):
+    """Save figure, or do nothing if savefig is False."""
+    if savefig:
+        neatplot.save_figure(name_str, file_type)
+
+
+def update_rc_params():
+    """Update matplotlib rc params."""
+    plt.rcParams.update({"font.size": 14})
+    plt.rcParams.update({"xtick.labelsize": 14})
+    plt.rcParams.update({"ytick.labelsize": 14})
+
+
+def make_plots(pred_mean, pred_std, idx1, idx2):
+    """Make set of plots."""
+
+    update_rc_params()
+    ylims = [-3, 3]
+    n_subset = 50
+
+    # Make xy plot
+    uviz.plot_xy(pred_mean, pred_std, y, x, n_subset=300, ylims=ylims, xlims=[0, 15])
+    save_figure(f"xy_{idx1}_{idx2}")
+    plt.show()
+
+    # Make intervals plot
+    uviz.plot_intervals(pred_mean, pred_std, y, n_subset=n_subset, ylims=ylims)
+    save_figure(f"intervals_{idx1}_{idx2}")
+    plt.show()
+
+    # Make calibration plot
+    uviz.plot_calibration(pred_mean, pred_std, y)
+    save_figure(f"calibration_{idx1}_{idx2}")
+    plt.show()
+
+    # Make ordered intervals plot
+    uviz.plot_intervals_ordered(pred_mean, pred_std, y, n_subset=n_subset, ylims=ylims)
+    save_figure(f"intervals_ordered_{idx1}_{idx2}")
+    plt.show()
+
+
+# List of predictive means and standard deviations
+pred_mean_list = [
+    f,
+    f + 0.1,
+    # f - 0.1,
+    # f + 0.25,
+    # f - 0.25,
+]
+
+pred_std_list = [
+    std * 0.5,  # overconfident
+    std * 2.0,  # underconfident
+    # std,                # correct
+]
+
+# Loop through, make plots, and compute metrics
+for i, pred_mean in enumerate(pred_mean_list):
+    for j, pred_std in enumerate(pred_std_list):
+        # Before recalibration
+        exp_props, obs_props = get_proportion_lists_vectorized(pred_mean, pred_std, y)
+        recal_model = None
+        mace = umetrics.mean_absolute_calibration_error(
+            pred_mean, pred_std, y, recal_model=recal_model
+        )
+        rmsce = umetrics.root_mean_squared_calibration_error(
+            pred_mean, pred_std, y, recal_model=recal_model
+        )
+        ma = umetrics.miscalibration_area(
+            pred_mean, pred_std, y, recal_model=recal_model
+        )
+        print("Before Recalibration")
+        print("   MACE: {:.3f}, RMSCE: {:.3f}, MA: {:.3f}".format(mace, rmsce, ma))
+
+        plot_calibration(
+            pred_mean, pred_std, y, exp_props=exp_props, obs_props=obs_props, show=True
+        )
+
+        # After recalibration
+        recal_model = iso_recal(exp_props, obs_props)
+        recal_exp_props, recal_obs_props = get_proportion_lists_vectorized(
+            pred_mean, pred_std, y, recal_model=recal_model
+        )
+        mace = umetrics.mean_absolute_calibration_error(
+            pred_mean, pred_std, y, recal_model=recal_model
+        )
+        rmsce = umetrics.root_mean_squared_calibration_error(
+            pred_mean, pred_std, y, recal_model=recal_model
+        )
+        ma = umetrics.miscalibration_area(
+            pred_mean, pred_std, y, recal_model=recal_model
+        )
+        print(" After Recalibration")
+        print("   MACE: {:.3f}, RMSCE: {:.3f}, MA: {:.3f}".format(mace, rmsce, ma))
+
+        plot_calibration(
+            pred_mean,
+            pred_std,
+            y,
+            exp_props=recal_exp_props,
+            obs_props=recal_obs_props,
+            show=True,
+        )
--- uncertainty_toolbox/recalibration.py
+++ uncertainty_toolbox/recalibration.py
@@ -18,7 +18,7 @@ def get_q_idx(exp_props, q):
             target_idx = idx
             break
     if target_idx is None:
-        raise ValueError('q must be within exp_props')
+        raise ValueError("q must be within exp_props")
     return target_idx
 
 
@@ -32,57 +32,63 @@ def iso_recal(exp_props, obs_props):
     min_obs = np.min(obs_props)
     max_obs = np.max(obs_props)
 
-    iso_model = IsotonicRegression(increasing=True, out_of_bounds='clip')
+    iso_model = IsotonicRegression(increasing=True, out_of_bounds="clip")
     # just need observed prop values between 0 and 1
     # problematic if min_obs_p > 0 and max_obs_p < 1
     if not (min_obs == 0.0) and (max_obs == 1.0):
-        print('Obs props not ideal: from {} to {}'.format(min_obs, max_obs))
+        print("Obs props not ideal: from {} to {}".format(min_obs, max_obs))
 
     exp_0_idx = get_q_idx(exp_props, 0.0)
     exp_1_idx = get_q_idx(exp_props, 1.0)
-    within_01 = obs_props[exp_0_idx:exp_1_idx + 1]
+    within_01 = obs_props[exp_0_idx : exp_1_idx + 1]
 
     beg_idx, end_idx = None, None
     # Handle beg_idx
-    min_obs_below = np.min(obs_props[:exp_0_idx])
-    min_obs_within = np.min(within_01)
-    if min_obs_below < min_obs_within:
-        i = exp_0_idx - 1
-        while obs_props[i] > min_obs_below:
-            i -= 1
-        beg_idx = i
-    elif np.sum((within_01 == min_obs_within).astype(float)) > 1:
-        # multiple minima in within_01 ==> get last min idx
-        i = exp_1_idx - 1
-        while obs_props[i] > min_obs_within:
-            i -= 1
-        beg_idx = i
-    elif np.sum((within_01 == min_obs_within).astype(float)) == 1:
-        beg_idx = int(np.argmin(within_01) + exp_0_idx)
+    if exp_0_idx != 0:
+        min_obs_below = np.min(obs_props[:exp_0_idx])
+        min_obs_within = np.min(within_01)
+        if min_obs_below < min_obs_within:
+            i = exp_0_idx - 1
+            while obs_props[i] > min_obs_below:
+                i -= 1
+            beg_idx = i
+        elif np.sum((within_01 == min_obs_within).astype(float)) > 1:
+            # multiple minima in within_01 ==> get last min idx
+            i = exp_1_idx - 1
+            while obs_props[i] > min_obs_within:
+                i -= 1
+            beg_idx = i
+        elif np.sum((within_01 == min_obs_within).astype(float)) == 1:
+            beg_idx = int(np.argmin(within_01) + exp_0_idx)
+        else:
+            raise RuntimeError("Inspect input arrays, cannot set beginning index")
     else:
-        raise RuntimeError('Inspect input arrays, cannot set beginning index')
+        beg_idx = exp_0_idx
 
     # Handle end_idx
-    max_obs_above = np.max(obs_props[exp_1_idx + 1:])
-    max_obs_within = np.max(within_01)
-    if max_obs_above > max_obs_within:
-        i = exp_1_idx + 1
-        while obs_props[i] < max_obs_above:
-            i += 1
-        end_idx = i + 1
-    elif np.sum((within_01 == max_obs_within).astype(float)) > 1:
-        # multiple minima in within_01 ==> get last min idx
-        i = beg_idx
-        while obs_props[i] < max_obs_within:
-            i += 1
-        end_idx = i + 1
-    elif np.sum((within_01 == max_obs_within).astype(float)) == 1:
-        end_idx = int(exp_0_idx + np.argmax(within_01) + 1)
+    if exp_1_idx < obs_props.shape[0] - 1:
+        max_obs_above = np.max(obs_props[exp_1_idx + 1 :])
+        max_obs_within = np.max(within_01)
+        if max_obs_above > max_obs_within:
+            i = exp_1_idx + 1
+            while obs_props[i] < max_obs_above:
+                i += 1
+            end_idx = i + 1
+        elif np.sum((within_01 == max_obs_within).astype(float)) > 1:
+            # multiple minima in within_01 ==> get last min idx
+            i = beg_idx
+            while obs_props[i] < max_obs_within:
+                i += 1
+            end_idx = i + 1
+        elif np.sum((within_01 == max_obs_within).astype(float)) == 1:
+            end_idx = int(exp_0_idx + np.argmax(within_01) + 1)
+        else:
+            raise RuntimeError("Inspect input arrays, cannot set ending index")
     else:
-        raise RuntimeError('Inspect input arrays, cannot set ending index')
+        end_idx = exp_1_idx + 1
 
     if not end_idx > beg_idx:
-        raise RuntimeError('Ending index before beginning index')
+        raise RuntimeError("Ending index before beginning index")
 
     filtered_obs_props = obs_props[beg_idx:end_idx]
     filtered_exp_props = exp_props[beg_idx:end_idx]
@@ -90,14 +96,15 @@ def iso_recal(exp_props, obs_props):
     try:
         iso_model = iso_model.fit(filtered_obs_props, filtered_exp_props)
     except Exception:
-        raise RuntimeError('Failed to fit isotonic regression model')
+        raise RuntimeError("Failed to fit isotonic regression model")
 
     return iso_model
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     exp = np.linspace(-0.5, 1.5, 200)
     from copy import deepcopy
+
     obs = deepcopy(exp)
     obs[:80] = 0
     obs[-80:] = 1
