764e6556848a4c40825c9621a02c28012abde44b
willieneis <willie.neiswanger@gmail.com>
2021-03-20 18:53:06 -0700

    add examples/plotly, with README, requirements, and two initial scripts

---
+++ examples/plotly/README.md
@@ -0,0 +1,11 @@
+# Plotly Examples 
+
+This directory contains scripts showing examples of visualizations using
+[plotly](https://github.com/plotly/plotly.py).
+
+## Installation
+
+To install Uncertainty Toolbox with plotly dependencies, run:
+```bash
+$ pip install -r examples/plotly/requirements_plotly.txt
+```
---
+++ examples/plotly/viz_minimal_plotly.py
@@ -0,0 +1,32 @@
+"""
+Examples of code for visualizations.
+"""
+
+import numpy as np
+
+import uncertainty_toolbox.viz as uviz
+import uncertainty_toolbox.data as udata
+
+import neatplot
+
+neatplot.set_style()
+neatplot.update_rc("figure.dpi", 150)
+neatplot.update_rc("text.usetex", False)
+
+
+# Set random seed
+np.random.seed(11)
+
+# Generate synthetic predictive uncertainty results
+(y_pred, y_std, y_true, _) = udata.synthetic_arange_random()
+
+# Print details about the synthetic results
+print("* y_true: {}".format(y_true))
+print("* y_pred: {}".format(y_pred))
+print("* |y_true - y_pred|: {}".format(np.abs(y_true - y_pred)))
+print("* y_std: {}".format(y_std))
+
+# Plot
+uviz.plotly_intervals(y_pred, y_std, y_true, show=True)
+uviz.plotly_calibration(y_pred, y_std, y_true, show=True)
+uviz.plotly_intervals_ordered(y_pred, y_std, y_true, show=True)
