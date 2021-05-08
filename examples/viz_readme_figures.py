43db18d54e6718bf48117298def51df2688c8e79
willieneis <willie.neiswanger@gmail.com>
2021-05-07 18:11:03 -0700

    examples: update script to produce all three new readme figs

--- examples/viz_readme_figures.py
+++ examples/viz_readme_figures.py
@@ -12,7 +12,7 @@ import uncertainty_toolbox.viz as uviz
 import neatplot
 
 neatplot.set_style()
-neatplot.update_rc("text.usetex", False)  # Set to True for system latex
+neatplot.update_rc("text.usetex", True)  # Set to True for system latex
 
 
 # Set random seed
@@ -23,13 +23,7 @@ n_obs = 650
 f, std, y, x = udata.synthetic_sine_heteroscedastic(n_obs)
 
 # Save figure (set to True to save)
-savefig = False
-
-
-def save_figure(name_str, file_type="png"):
-    """Save figure, or do nothing if savefig is False."""
-    if savefig:
-        neatplot.save_figure(name_str, file_type)
+savefig = True
 
 
 def update_rc_params():
@@ -39,14 +33,14 @@ def update_rc_params():
     plt.rcParams.update({"ytick.labelsize": 14})
 
 
-def make_plots(pred_mean, pred_std, idx1, idx2):
+def make_plots(pred_mean, pred_std, plot_save_str='row'):
     """Make set of plots."""
 
     update_rc_params()
     ylims = [-3, 3]
     n_subset = 50
 
-    fig, axs = plt.subplots(1, 3, figsize=(19, 6))
+    fig, axs = plt.subplots(1, 3, figsize=(17, 8))
 
     # Make xy plot
     axs[0] = uviz.plot_xy_ax(
@@ -58,22 +52,17 @@ def make_plots(pred_mean, pred_std, idx1, idx2):
         pred_mean, pred_std, y, n_subset=n_subset, ylims=ylims, ax=axs[1]
     )
 
-    ## Make intervals plot
-    #uviz.plot_intervals(pred_mean, pred_std, y, n_subset=n_subset, ylims=ylims)
-    #save_figure(f"intervals_{idx1}_{idx2}")
-
-    ## Make calibration plot
-    #uviz.plot_calibration(pred_mean, pred_std, y)
-    #save_figure(f"calibration_{idx1}_{idx2}")
-    #plt.show()
+    # Make calibration plot
+    axs[2] = uviz.plot_calibration_ax(pred_mean, pred_std, y, ax=axs[2])
 
     # Adjust subplots spacing
     plt.subplots_adjust(
-        left=None, bottom=None, right=None, top=None, wspace=0.2, hspace=None
+        left=None, bottom=None, right=None, top=None, wspace=0.25, hspace=None
     )
 
     # Save figure
-    fig.savefig('test_row.svg')
+    if savefig:
+        neatplot.save_figure(plot_save_str, ['svg', 'pdf'])
 
 
 # List of predictive means and standard deviations
@@ -81,17 +70,19 @@ pred_mean_list = [f]
 
 pred_std_list = [
     std * 0.5,  # overconfident
-    #std * 2.0,  # underconfident
-    #std,  # correct
+    std * 2.0,  # underconfident
+    std,  # correct
 ]
 
 # Loop through, make plots, and compute metrics
+idx_counter = 0
 for i, pred_mean in enumerate(pred_mean_list):
     for j, pred_std in enumerate(pred_std_list):
         mace = umetrics.mean_absolute_calibration_error(pred_mean, pred_std, y)
         rmsce = umetrics.root_mean_squared_calibration_error(pred_mean, pred_std, y)
         ma = umetrics.miscalibration_area(pred_mean, pred_std, y)
 
-        make_plots(pred_mean, pred_std, i, j)
+        idx_counter += 1
+        make_plots(pred_mean, pred_std, f'row_{idx_counter}')
 
         print(f"MACE: {mace}, RMSCE: {rmsce}, MA: {ma}")
