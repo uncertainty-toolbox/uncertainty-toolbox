4ec693e98d43fb6f40261f59cdace171d49ffd3c
willieneis <willie.neiswanger@gmail.com>
2021-05-07 18:46:34 -0700

    update neatplot details

--- neatplot/__init__.py
+++ neatplot/__init__.py
@@ -5,24 +5,34 @@ Neat plotting utilities for Python.
 import pathlib
 import matplotlib.pyplot as plt
 
+
 def set_style(style_str='default'):
     if style_str == 'default':
         plt.style.use((pathlib.Path(__file__).parent / 'matplotlibrc').resolve())
     elif style_str == 'fonts':
         plt.style.use((pathlib.Path(__file__).parent / 'matplotlibrc_fonts').resolve())
 
-def save_figure(file_name='figure', ext_list = ['pdf', 'png'], white_background=True):
+
+def save_figure(file_name='figure', ext_list=None, white_background=True):
     """Save figure for all extensions in ext_list."""
 
+    # Default ext_list
+    if ext_list is None:
+        ext_list = ['pdf', 'png']
+
+    # If ext_list is a single str
     if isinstance(ext_list, str):
         ext_list = [ext_list]
 
+    # Set facecolor and edgecolor
     (fc, ec) = ('w', 'w') if white_background else ('none', 'none')
 
+    # Save each type in ext_list
     for ext in ext_list:
         save_str = file_name + '.' + ext
         plt.savefig(save_str, bbox_inches='tight', facecolor=fc, edgecolor=ec)
         print(f'Saved figure {save_str}')
+
 
 def update_rc(key_str, value):
     """Update matplotlib rc params."""
