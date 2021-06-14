b78bbdf07eb07c949d06bd3debbb41eb59631fc6
willieneis <willie.neiswanger@gmail.com>
2021-06-14 02:52:59 -0700

    examples: add more plots to viz_minimal script

--- examples/viz_minimal.py
+++ examples/viz_minimal.py
@@ -20,22 +20,47 @@ neatplot.update_rc("text.usetex", False)
 np.random.seed(11)
 
 # Generate synthetic predictive uncertainty results
-(y_pred, y_std, y_true, _) = udata.synthetic_arange_random()
+(y_pred, y_std, y_true, x) = udata.synthetic_arange_random(20)
+
+# Plot xy
+fig, ax = plt.subplots(1, 1, figsize=(5, 5))
+uviz.plot_xy(y_pred, y_std, y_true, x, leg_loc=4, ax=ax)
+neatplot.save_figure('viz_minimal_00', 'png')
+plt.show()
 
 # Plot intervals
 fig, ax = plt.subplots(1, 1, figsize=(5, 5))
 uviz.plot_intervals(y_pred, y_std, y_true, ax=ax)
-neatplot.save_figure('viz_minimal_01', 'svg')
-plt.show()
-
-# Plot calibration
-fig, ax = plt.subplots(1, 1, figsize=(5, 5))
-uviz.plot_calibration(y_pred, y_std, y_true, ax=ax)
-neatplot.save_figure('viz_minimal_02', 'svg')
+neatplot.save_figure('viz_minimal_01', 'png')
 plt.show()
 
 # Plot intervals_ordered
 fig, ax = plt.subplots(1, 1, figsize=(5, 5))
 uviz.plot_intervals_ordered(y_pred, y_std, y_true, ax=ax)
-neatplot.save_figure('viz_minimal_03', 'svg')
+neatplot.save_figure('viz_minimal_02', 'png')
+plt.show()
+
+# Plot calibration
+fig, ax = plt.subplots(1, 1, figsize=(5, 5))
+uviz.plot_calibration(y_pred, y_std, y_true, ax=ax)
+neatplot.save_figure('viz_minimal_03', 'png')
+plt.show()
+
+# Plot adversarial group calibration
+fig, ax = plt.subplots(1, 1, figsize=(6, 4))
+uviz.plot_adversarial_group_calibration(y_pred, y_std, y_true, ax=ax)
+neatplot.save_figure('viz_minimal_04', 'png')
+plt.show()
+
+# Plot sharpness
+fig, ax = plt.subplots(1, 1, figsize=(5, 5))
+uviz.plot_sharpness(y_std, ax=ax)
+neatplot.save_figure('viz_minimal_05', 'png')
+plt.show()
+
+# Plot residuals vs stds
+fig, ax = plt.subplots(1, 1, figsize=(5, 5))
+y_resid = y_true - y_pred
+uviz.plot_residuals_vs_stds(y_resid, y_std, ax=ax)
+neatplot.save_figure('viz_minimal_06', 'png')
 plt.show()
