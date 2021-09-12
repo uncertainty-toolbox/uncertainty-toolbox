5d5e17e96028a4882bc8ac0894d9243dc611c545
Youngseog Chung <youngsec@cs.cmu.edu>
2021-09-12 13:00:24 -0400

    added group calibration

--- uncertainty_toolbox/metrics_calibration_quantile.py
+++ uncertainty_toolbox/metrics_calibration_quantile.py
@@ -1,5 +1,31 @@
+from argparse import Namespace
 import numpy as np
 import torch
+from shapely.geometry import Polygon, LineString
+from shapely.ops import polygonize, unary_union
+from tqdm import tqdm
+
+
+def quantile_sharpness(
+    model,
+    x,
+    recal_model=None,
+    recal_type=None,
+):
+    exp_proportions = np.array([0.025, 0.975])
+    quantile_predictions = get_quantile_model_predictions(
+        model=model,
+        x=x,
+        exp_proportions=exp_proportions,
+        recal_model=recal_model,
+        recal_type=recal_type,
+    )
+    q_025 = quantile_predictions[:, 0]
+    q_975 = quantile_predictions[:, 1]
+    sharp_metric = np.mean(q_975 - q_025)
+
+    return sharp_metric
+
 
 def get_quantile_model_predictions(
     model,
@@ -34,6 +60,7 @@ def quantile_root_mean_squared_calibration_error(
     num_bins=99,
     recal_model=None,
     recal_type=None,
+    prop_type="quantile",
 ):
     if isinstance(y, torch.Tensor):
         y = y.detach().cpu().numpy()
@@ -62,6 +89,7 @@ def quantile_mean_absolute_calibration_error(
     num_bins=99,
     recal_model=None,
     recal_type=None,
+    prop_type="quantile",
 ):
     if isinstance(y, torch.Tensor):
         y = y.detach().cpu().numpy()
@@ -84,13 +112,19 @@ def quantile_mean_absolute_calibration_error(
     return mace
 
 
-def quantile_sharpness(
+def quantile_miscalibration_area(
     model,
     x,
+    y,
+    num_bins=99,
     recal_model=None,
     recal_type=None,
+    prop_type="quantile",
 ):
-    exp_proportions = np.array([0.025, 0.975])
+    if isinstance(y, torch.Tensor):
+        y = y.detach().cpu().numpy()
+
+    exp_proportions = np.linspace(0.01, 0.99, num_bins)
     quantile_predictions = get_quantile_model_predictions(
         model=model,
         x=x,
@@ -98,11 +132,93 @@ def quantile_sharpness(
         recal_model=recal_model,
         recal_type=recal_type,
     )
-    q_025 = quantile_predictions[:, 0]
-    q_975 = quantile_predictions[:, 1]
-    sharp_metric = np.mean(q_975 - q_025)
 
-    return sharp_metric
+    obs_proportions = np.mean((quantile_predictions >= y).astype(float), axis=0).flatten()
+    assert exp_proportions.shape == obs_proportions.shape
+
+    # Compute approximation to area between curves
+    polygon_points = []
+    for point in zip(exp_proportions, obs_proportions):
+        polygon_points.append(point)
+    for point in zip(reversed(exp_proportions), reversed(exp_proportions)):
+        polygon_points.append(point)
+    polygon_points.append((exp_proportions[0], obs_proportions[0]))
+    polygon = Polygon(polygon_points)
+    x, y = polygon.exterior.xy
+    ls = LineString(np.c_[x, y])
+    lr = LineString(ls.coords[:] + ls.coords[0:1])
+    mls = unary_union(lr)
+    polygon_area_list = [poly.area for poly in polygonize(mls)]
+    miscalibration_area = np.asarray(polygon_area_list).sum()
+
+    return miscalibration_area
+
+
+def quantile_adversarial_group_calibration(
+    model,
+    x,
+    y,
+    cali_type,
+    prop_type="quantile",
+    num_bins=99,
+    num_group_bins=10,
+    draw_with_replacement=False,
+    num_trials=10,
+    num_group_draws=10,
+    verbose=False,
+):
+
+    if cali_type == "mean_abs":
+        cali_fn = quantile_mean_absolute_calibration_error
+    elif cali_type == "root_mean_sq":
+        cali_fn = quantile_root_mean_squared_calibration_error
+
+    num_pts = y.shape[0]
+    ratio_arr = np.linspace(0, 1, num_group_bins)
+    score_mean_per_ratio = []
+    score_stderr_per_ratio = []
+    if verbose:
+        print(
+            (
+                "Measuring adversarial group calibration by spanning group"
+                " size between {} and {}, in {} intervals"
+            ).format(np.min(ratio_arr), np.max(ratio_arr), num_group_bins)
+        )
+    progress = tqdm(ratio_arr) if verbose else ratio_arr
+    for r in progress:
+        group_size = max([int(round(num_pts * r)), 2])
+        score_per_trial = []  # list of worst miscalibrations encountered
+        for _ in range(num_trials):
+            group_miscal_scores = []
+            for g_idx in range(num_group_draws):
+                rand_idx = np.random.choice(
+                    num_pts, group_size, replace=draw_with_replacement
+                )
+                group_x = x[rand_idx]
+                group_y = y[rand_idx]
+                group_miscal = cali_fn(
+                    model,
+                    group_x,
+                    group_y,
+                    num_bins=num_bins,
+                    prop_type=prop_type,
+                )
+                group_miscal_scores.append(group_miscal)
+            max_miscal_score = np.max(group_miscal_scores)
+            score_per_trial.append(max_miscal_score)
+        score_mean_across_trials = np.mean(score_per_trial)
+        score_stderr_across_trials = np.std(score_per_trial, ddof=1)
+        score_mean_per_ratio.append(score_mean_across_trials)
+        score_stderr_per_ratio.append(score_stderr_across_trials)
+
+    out = Namespace(
+        group_size=ratio_arr,
+        score_mean=np.array(score_mean_per_ratio),
+        score_stderr=np.array(score_stderr_per_ratio),
+    )
+
+    return out
+
 
 
 # def test_uq(
