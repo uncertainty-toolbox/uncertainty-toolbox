5825bba9a25daf46eb340e950986d57cc1f5eec9
Willie Neiswanger <willie.neiswanger@gmail.com>
2021-09-19 15:25:13 -0700

    Setup fixes (#53)
    * update setup.py with package_data
    
    * update requirements files
    
    * update flag in shell script to run all tests

--- setup.py
+++ setup.py
@@ -17,6 +17,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/uncertainty-toolbox/uncertainty-toolbox",
     packages=setuptools.find_packages(),
+    package_data={"uncertainty_toolbox": ["matplotlibrc"]},
     install_requires=requirements,
     classifiers=[
         "Programming Language :: Python :: 3",
