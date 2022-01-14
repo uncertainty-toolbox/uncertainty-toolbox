e8fcc407f1048d6a68ca800921c8716e470a9294
sgbaird <sterling.baird@icloud.com>
2022-01-13 19:50:27 -0700

    update description (use the one from setup.py)

--- uncertainty_toolbox/__init__.py
+++ uncertainty_toolbox/__init__.py
@@ -1,4 +1,4 @@
-"""Code for the Uncertainty Toolbox"""
+"""A python toolbox for predictive uncertainty quantification, calibration, metrics, and visualization."""
 __version__ = "0.1.0"
 
 from .data import (
