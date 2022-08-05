b0b22a14507be3756dd229ca6b6f11cdeb97d8c5
willieneis <willie.neiswanger@gmail.com>
2022-08-04 22:19:04 -0700

    update pyproject and __init__.py docstring

--- uncertainty_toolbox/__init__.py
+++ uncertainty_toolbox/__init__.py
@@ -1,4 +1,5 @@
-"""A python toolbox for predictive uncertainty quantification, calibration, metrics, and visualization."""
+"""Uncertainty Toolbox: a python toolbox for predictive uncertainty quantification, calibration, metrics, and visualization."""
+
 __version__ = "0.1.0"
 
 from .data import (
